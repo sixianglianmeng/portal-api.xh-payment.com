07c721c7e4cd6778da15de25309e83a4902942d4
kerr8819 <kerr8819@gmail.com>
2018-08-18 03:06:32 +0800

    tiaoshi

--- protected/modules/api/controllers/v1/AccountController.php
+++ protected/modules/api/controllers/v1/AccountController.php
@@ -632,7 +632,7 @@ class AccountController extends BaseController
         $sorts = [
             'created_at-'=>['created_at',SORT_DESC],
         ];
-        Yii::info('merchant_account-----',json_encode($this->allParams));
+        Yii::info('merchant_account-----',$this->allParams);
         $sort = ControllerParameterValidator::getRequestParam($this->allParams, 'sort', 15, Macro::CONST_PARAM_TYPE_SORT, '分页参数错误',[1,100]);
         $perPage = ControllerParameterValidator::getRequestParam($this->allParams, 'limit', Macro::PAGINATION_DEFAULT_PAGE_SIZE, Macro::CONST_PARAM_TYPE_INT_GT_ZERO, '分页参数错误',[1,100]);
         $page = ControllerParameterValidator::getRequestParam($this->allParams, 'page', 1, Macro::CONST_PARAM_TYPE_INT_GT_ZERO, '分页参数错误',[1,1000]);
