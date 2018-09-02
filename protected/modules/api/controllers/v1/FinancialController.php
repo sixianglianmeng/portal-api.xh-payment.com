ca8c12a173b849d5172bb16471036f4796bc741f
bootmall <bootmall@gmail.com>
2018-09-02 19:28:58 +0800

    修复管理员帐变列表商户id查询bug

--- protected/modules/api/controllers/v1/FinancialController.php
+++ protected/modules/api/controllers/v1/FinancialController.php
@@ -61,7 +61,7 @@ class FinancialController extends BaseController
         $dateStart = ControllerParameterValidator::getRequestParam($this->allParams, 'dateStart', '',Macro::CONST_PARAM_TYPE_DATE,'开始日期错误');
         $dateEnd = ControllerParameterValidator::getRequestParam($this->allParams, 'dateEnd', '',Macro::CONST_PARAM_TYPE_DATE,'结束日期错误');
 
-        $uid = ControllerParameterValidator::getRequestParam($this->allParams, 'uid','',Macro::CONST_PARAM_TYPE_INT,'商户号错误',[0,100]);
+        $uid = ControllerParameterValidator::getRequestParam($this->allParams, 'uid','',Macro::CONST_PARAM_TYPE_INT,'商户号错误');
 
         $username = ControllerParameterValidator::getRequestParam($this->allParams, 'username','',Macro::CONST_PARAM_TYPE_STRING,'商户账号号错误',[6,16]);
 
