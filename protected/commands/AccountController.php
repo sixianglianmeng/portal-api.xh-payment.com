bc08a9a142741fbaaf1c495045a05e43e9880950
bootmall <bootmall@gmail.com>
2018-09-11 22:16:25 +0800

    更新每个用户新增的网银内冲支付方式配置

--- protected/commands/AccountController.php
+++ protected/commands/AccountController.php
@@ -3,6 +3,7 @@
 namespace app\commands;
 
 use app\common\models\model\ChannelAccount;
+use app\common\models\model\MerchantRechargeMethod;
 use app\common\models\model\User;
 use app\common\models\model\UserPaymentInfo;
 use app\components\Util;
@@ -78,5 +79,46 @@ class AccountController extends BaseConsoleCommand
         $user->save();
 
         $user->setGroupRole();
+    }
+
+    /**
+     * 更新每个用户新增的网银内冲支付方式配置
+     */
+    public function actionUpdateUserWync(){
+
+        $sql = "select u.parent_agent_id,m.* from p_merchant_recharge_methods m, p_users u where m.merchant_id=u.id and m.method_id='WY' and u.parent_agent_id>0 and u.group_id=30 and m.fee_rate>0";
+        //$sql .=" AND u.id=3012186";
+        $merchantMethods = Yii::$app->db->createCommand($sql)->queryAll();
+        foreach ($merchantMethods as $mm) {
+            $_SERVER['LOG_ID'] = strval(uniqid());
+            Yii::info($_SERVER['LOG_ID'].' updateUserWync '.$mm['merchant_account'].' '.$mm['fee_rate']);
+            $userPaymentInfo = UserPaymentInfo::findOne(['user_id' => $mm['merchant_id']]);
+            $parent = User::findOne(['id' => $mm['parent_agent_id']]);
+
+            $mm['method_id'] = 'WYNC';
+            $mm['method_name'] = '网银内充';
+            $mm['created_at'] = time();
+            $mm['updated_at'] =0;
+
+            $methodConfig = MerchantRechargeMethod::find()->where(['method_id'=>$mm['method_id'],'app_id'=>$mm['merchant_id']])->limit(1)->one();
+            if($methodConfig){
+                continue;
+            }
+            $methodConfig = new MerchantRechargeMethod();
+
+            unset($mm['id']);
+            unset($mm['parent_recharge_rebate_rate']);
+            unset($mm['parent_method_config_id']);
+            unset($mm['all_parent_method_config_id']);
+            $methodConfig->setAttributes($mm,false);
+            $methodConfig->save();
+
+            $pm['id'] = 'WYNC';
+            $pm['status'] = '1';
+            $pm['rate'] = $mm['fee_rate'];
+            $payMethods = [$pm];
+
+            $userPaymentInfo->updatePayMethods($parent,$payMethods);
+        }
     }
 }
