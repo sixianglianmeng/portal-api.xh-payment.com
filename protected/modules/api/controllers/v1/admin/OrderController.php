2f42f050415977a2d5559d0c6cacacb3a31f1a81
kerr8819 <kerr8819@gmail.com>
2018-09-14 02:36:26 +0800

    调整充值金额走势（按小时）

--- protected/modules/api/controllers/v1/admin/OrderController.php
+++ protected/modules/api/controllers/v1/admin/OrderController.php
@@ -363,67 +363,6 @@ class OrderController extends BaseController
             }
 
         }
-
-
         return ResponseHelper::formatOutput(Macro::SUCCESS, 'IP及设备ID成功加入黑名单');
     }
-
-    /**
-     * 充值金额统计分析
-     * @return array
-     * @throws \power\yii2\exceptions\ParameterValidationExpandException
-     */
-    public function actionEcharts()
-    {
-        $dateStart = ControllerParameterValidator::getRequestParam($this->allParams, 'dateStart', '',Macro::CONST_PARAM_TYPE_DATE,'开始日期错误');
-        $dateEnd = ControllerParameterValidator::getRequestParam($this->allParams, 'dateEnd', '',Macro::CONST_PARAM_TYPE_DATE,'结束日期错误');
-        $startTime = empty($dateStart) ? strtotime('-4 days',strtotime(date("Y-m-d"))) : strtotime(date("Y-m-d",strtotime($dateStart)));
-        $endTime = empty($dateEnd) ? strtotime(date("Y-m-d")) : strtotime(date("Y-m-d",strtotime($dateEnd)));
-        $days = ($endTime - $startTime) / (24*3600) ;
-        if($days > 4){
-            return ResponseHelper::formatOutput(Macro::ERR_UNKNOWN, '时间筛选跨度不能超过4天');
-        }
-        $timeArray = [
-            0=>['date_start'=>'00:00:00','date_end'=>'00:59:59'],
-            1=>['date_start'=>'01:00:00','date_end'=>'01:59:59'],
-            2=>['date_start'=>'02:00:00','date_end'=>'02:59:59'],
-            3=>['date_start'=>'03:00:00','date_end'=>'03:59:59'],
-            4=>['date_start'=>'04:00:00','date_end'=>'04:59:59'],
-            5=>['date_start'=>'05:00:00','date_end'=>'05:59:59'],
-            6=>['date_start'=>'06:00:00','date_end'=>'06:59:59'],
-            7=>['date_start'=>'07:00:00','date_end'=>'07:59:59'],
-            8=>['date_start'=>'08:00:00','date_end'=>'08:59:59'],
-            9=>['date_start'=>'09:00:00','date_end'=>'09:59:59'],
-            10=>['date_start'=>'10:00:00','date_end'=>'10:59:59'],
-            11=>['date_start'=>'11:00:00','date_end'=>'11:59:59'],
-            12=>['date_start'=>'12:00:00','date_end'=>'12:59:59'],
-            13=>['date_start'=>'13:00:00','date_end'=>'13:59:59'],
-            14=>['date_start'=>'14:00:00','date_end'=>'14:59:59'],
-            15=>['date_start'=>'15:00:00','date_end'=>'15:59:59'],
-            16=>['date_start'=>'16:00:00','date_end'=>'16:59:59'],
-            17=>['date_start'=>'17:00:00','date_end'=>'17:59:59'],
-            18=>['date_start'=>'18:00:00','date_end'=>'18:59:59'],
-            19=>['date_start'=>'19:00:00','date_end'=>'19:59:59'],
-            20=>['date_start'=>'20:00:00','date_end'=>'20:59:59'],
-            21=>['date_start'=>'21:00:00','date_end'=>'21:59:59'],
-            22=>['date_start'=>'22:00:00','date_end'=>'22:59:59'],
-            23=>['date_start'=>'23:00:00','date_end'=>'23:59:59']
-        ];
-        $data = [];
-        for($i = 0 ;$i < $days ;$i++){
-            $dayTime = date("Y-m-d",strtotime("-{$i} days"));
-            foreach ($timeArray as $key => $val){
-                $where['start_time'] = $dayTime.' '.$val['date_start'];
-                $where['end_time'] = $dayTime.' '.$val['date_end'];
-                $tmp = Order::totalChargeAmount($where);
-                $data[$dayTime][] = $tmp;
-            }
-        }
-//        $data['2018-09-11'] = ["543113.69", "673987.85", "466886.15", "475646.00", "189140.00", "67255.92", "180326.07", "1178641.40", "2450301.12", "3045451.78", "3452237.29", "3708345.14", "2919407.97", "2357965.43", "3117303.52", "3537998.93", "3218793.33", "2107883.30", "2294047.92", "2206863.96", "2150130.50", "2142737.45", "1507022.58", "1157057.98"];
-//        $data['2018-09-12'] = ["1024946.62", "913214.68", "786174.50", "177659.00", "268266.00", "61409.00", "186080.46", "945199.99", "2331624.77", "3165531.71", "3981066.28", "2976927.33", "2565486.38", "3743021.67", "2904412.55", "3080567.71", "3212351.72", "2853842.80", "3158698.65", "3526467.97", "2048941.09", "1786266.44", "1304818.20", "997392.97"];
-//        $data['2018-09-13'] = ["1042847.62", "324156.39", "324878.52", "180085.00", "214001.75", "177087.90", "259895.04", "1330923.61", "3049007.72", "2956231.97", "3075395.27", "3778228.01", "3341839.14", "4529695.05", "4024294.83", "4185795.08", "3356373.75", "3356332.78", "3016910.81", "3017963.43", "2795871.18", "1320512.08", "1887223.95", "1524706.97"];
-//        $data['2018-09-14'] = ["1017278.55", "42579.12", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"];
-        return ResponseHelper::formatOutput(Macro::SUCCESS,'',$data);
-    }
-
 }
