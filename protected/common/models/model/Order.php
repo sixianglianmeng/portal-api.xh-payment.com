42c84e986b324a76c8f77a095b9a72a3b2b1cc88
kerr8819 <kerr8819@gmail.com>
2018-09-14 03:32:36 +0800

    调整充值金额走势

--- protected/common/models/model/Order.php
+++ protected/common/models/model/Order.php
@@ -198,19 +198,4 @@ class Order extends BaseModel
 
         return $order[0];
     }
-
-    /**
-     * 统计每小时充值金额
-     * @param $where
-     * @return int|mixed
-     */
-    public static function totalChargeAmount($where)
-    {
-        $query = self::find();
-        $query->andFilterCompare('settlement_at','>='.strtotime($where['start_time']));
-        $query->andFilterCompare('settlement_at','<='.strtotime($where['end_time']));
-        $query->andWhere(['status'=>self::STATUS_SETTLEMENT]);
-        $amount = $query->sum('paid_amount');
-        return empty($amount) ? 0 : $amount;
-    }
 }
--- protected/modules/api/controllers/v1/admin/EchartsController.php
+++ protected/modules/api/controllers/v1/admin/EchartsController.php
@@ -25,47 +25,26 @@ class EchartsController extends BaseController
     {
         $dateStart = ControllerParameterValidator::getRequestParam($this->allParams, 'dateStart', '',Macro::CONST_PARAM_TYPE_DATE,'开始日期错误');
         $dateEnd = ControllerParameterValidator::getRequestParam($this->allParams, 'dateEnd', '',Macro::CONST_PARAM_TYPE_DATE,'结束日期错误');
-        $startTime = empty($dateStart) ? strtotime('-4 days',strtotime(date("Y-m-d"))) : strtotime(date("Y-m-d",strtotime($dateStart)));
-        $endTime = empty($dateEnd) ? strtotime(date("Y-m-d")) : strtotime(date("Y-m-d",strtotime($dateEnd)));
+        $startTime = empty($dateStart) ? strtotime('-4 days',strtotime(date("Y-m-d 00:00:00"))) : strtotime(date("Y-m-d 00:00:00",strtotime($dateStart)));
+        $endTime = empty($dateEnd) ? strtotime(date("Y-m-d 23:59:59")) : strtotime(date("Y-m-d 23:59:59",strtotime($dateEnd)));
         $days = ($endTime - $startTime) / (24*3600) ;
         if($days > 4){
             return ResponseHelper::formatOutput(Macro::ERR_UNKNOWN, '时间筛选跨度不能超过4天');
         }
-        $timeArray = [
-            0=>['date_start'=>'00:00:00','date_end'=>'00:59:59'],
-            1=>['date_start'=>'01:00:00','date_end'=>'01:59:59'],
-            2=>['date_start'=>'02:00:00','date_end'=>'02:59:59'],
-            3=>['date_start'=>'03:00:00','date_end'=>'03:59:59'],
-            4=>['date_start'=>'04:00:00','date_end'=>'04:59:59'],
-            5=>['date_start'=>'05:00:00','date_end'=>'05:59:59'],
-            6=>['date_start'=>'06:00:00','date_end'=>'06:59:59'],
-            7=>['date_start'=>'07:00:00','date_end'=>'07:59:59'],
-            8=>['date_start'=>'08:00:00','date_end'=>'08:59:59'],
-            9=>['date_start'=>'09:00:00','date_end'=>'09:59:59'],
-            10=>['date_start'=>'10:00:00','date_end'=>'10:59:59'],
-            11=>['date_start'=>'11:00:00','date_end'=>'11:59:59'],
-            12=>['date_start'=>'12:00:00','date_end'=>'12:59:59'],
-            13=>['date_start'=>'13:00:00','date_end'=>'13:59:59'],
-            14=>['date_start'=>'14:00:00','date_end'=>'14:59:59'],
-            15=>['date_start'=>'15:00:00','date_end'=>'15:59:59'],
-            16=>['date_start'=>'16:00:00','date_end'=>'16:59:59'],
-            17=>['date_start'=>'17:00:00','date_end'=>'17:59:59'],
-            18=>['date_start'=>'18:00:00','date_end'=>'18:59:59'],
-            19=>['date_start'=>'19:00:00','date_end'=>'19:59:59'],
-            20=>['date_start'=>'20:00:00','date_end'=>'20:59:59'],
-            21=>['date_start'=>'21:00:00','date_end'=>'21:59:59'],
-            22=>['date_start'=>'22:00:00','date_end'=>'22:59:59'],
-            23=>['date_start'=>'23:00:00','date_end'=>'23:59:59']
-        ];
+        $query = Order::find();
+        $query->andFilterCompare('settlement_at','>='.$startTime);
+        $query->andFilterCompare('settlement_at','<='.$endTime);
+        $query->andWhere(['status'=>Order::STATUS_SETTLEMENT]);
+//        $query->groupBy("from_unixtime(`settlement_at`,'%Y%m%d%H')");
+        $query->groupBy = "from_unixtime(`settlement_at`,'%Y%m%d %H')";
+        $query->select("sum(`paid_amount`) as amount,from_unixtime(settlement_at,'%Y%m%d%H') as times");
+        $list = $query->asArray()->all();
+        $sql = $query->createCommand()->getRawSql();
         $data = [];
-        for($i = 0 ;$i < $days ;$i++){
-            $dayTime = date("Y-m-d",strtotime("-{$i} days"));
-            foreach ($timeArray as $key => $val){
-                $where['start_time'] = $dayTime.' '.$val['date_start'];
-                $where['end_time'] = $dayTime.' '.$val['date_end'];
-                $tmp = Order::totalChargeAmount($where);
-                $data[$dayTime][] = $tmp;
-            }
+//        if (!$list) return ResponseHelper::formatOutput(Macro::SUCCESS,$sql);
+        foreach ($list as $val){
+            $tmp = explode(' ' ,$val['times']);
+            $data[$tmp[0]][$tmp[1]] = $val['amount'];
         }
         return ResponseHelper::formatOutput(Macro::SUCCESS,'',$data);
     }
