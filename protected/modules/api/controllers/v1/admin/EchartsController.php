eff5ef9fff434e57fe9ab3eac15ae920786289e8
tomcat3389 <tomcat3389@gmail.com>
2018-09-15 02:21:34 +0800

    header

--- protected/modules/api/controllers/v1/admin/EchartsController.php
+++ protected/modules/api/controllers/v1/admin/EchartsController.php
@@ -58,6 +58,6 @@ class EchartsController extends BaseController
 //                $data[$key][] = $value;
 //            }
 //        }
-        return ResponseHelper::formatOutput(Macro::SUCCESS,'',$data);
+        return ResponseHelper::formatOutput(Macro::SUCCESS,'',$chartData);
     }
 }
