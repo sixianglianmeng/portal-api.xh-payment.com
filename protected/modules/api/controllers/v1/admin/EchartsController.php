2d2f6ca5956c6968314616a42e514095488fff16
kerr8819 <kerr8819@gmail.com>
2018-09-14 22:50:58 +0800

    修改充值金额走势查询sql

--- protected/modules/api/controllers/v1/admin/EchartsController.php
+++ protected/modules/api/controllers/v1/admin/EchartsController.php
@@ -45,7 +45,7 @@ class EchartsController extends BaseController
         foreach ($list as $val){
             $tmp = ['00' => 0 , '01' => 0 , '02' => 0 , '03' => 0 , '04' => 0 , '05' => 0 , '06' => 0 , '07' => 0 , '08' => 0 , '09' => 0 , '10' => 0 , '11' => 0 , '12' => 0 , '13' => 0,'14' => 0 ,'15' => 0 ,'16' => 0 ,'17' => 0 ,'18' => 0 ,'19' => 0 ,'20' => 0,'21' => 0 ,'22' => 0 ,'23' => 0];
             list($date,$time) = explode(" ",$val['times']);
-            $tmp[(string)$time] = $val['amount'];
+            $tmp[$time] = $val['amount'];
             $chartData[$date] = $tmp;
         }
         $data = [];
